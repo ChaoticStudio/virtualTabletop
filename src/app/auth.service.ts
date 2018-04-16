914bd1172d33a68144380079dd2087467c281405
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2018-04-16 16:56:33 -0300

    logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
@@ -7,7 +8,8 @@ export class AuthService {
   private _registerUrl = '/api/register';
   private _loginUrl = '/api/login';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private _router: Router) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user);
@@ -19,6 +21,11 @@ export class AuthService {
 
   loggedIn() {
     return !!localStorage.getItem('token');
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/home']);
   }
 
   getToken() {
