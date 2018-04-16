1b25e568f1eb5c436ac42b7b9e94b0dc25cf4263
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2018-04-16 15:39:45 -0300

    generate jwt at back-end

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+    jwt = require('jsonwebtoken'),
     router = express.Router(),
     mongoose = require('mongoose'),
     Message = require('../modules/message'),
@@ -21,7 +22,9 @@ router.post('/register', (req, res) => {
         if(error) {
             console.log(error);
         } else {
-            res.status(200).send(registeredUser);
+            let payload = { subject: registeredUser._id };
+            let token = jwt.sign(payload, 'secretKey');            
+            res.status(200).send({token});
         }
     });
 });
@@ -37,7 +40,9 @@ router.post('/login', (req, res) => {
             } else if (user.password !== userData.password) {
                 res.status(401).send('Invalid password');
             } else {
-                res.status(200).send(user);
+                let payload = { subject: user._id };
+                let token = jwt.sign(payload, 'secretKey');
+                res.status(200).send({token});
             }
         }
     });
