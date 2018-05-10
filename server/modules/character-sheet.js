39b329f33bfad2115c0d3de345016e4063e7c699
MMoserM <michi.celin@gmail.com>
2018-05-09 21:55:53 -0300

    Charcter Sheet Added Character Sheet

--- server/modules/character-sheet.js
+++ server/modules/character-sheet.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-const characterSheetSchema = new Schema({ sheet: {} });
+const characterSheetSchema = new Schema({
+    owner: String,
+    sheet: {}
+});
 
 module.exports = mongoose.model('characterSheet', characterSheetSchema, 'characterSheets');
--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -12,7 +12,6 @@ import * as socketIo from 'socket.io-client';
 export class CharacterSheetComponent implements OnInit {
 
   characterSheetObject: Object;
-  debug;
   characterSheetKeys: Array<String>;
 
   constructor(private _characterSheetService: CharacterSheetService) { }
@@ -26,7 +25,6 @@ export class CharacterSheetComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._characterSheetService.getCharacterSheet('test').subscribe((resData) => this.loadFields(resData));
   }
 
   loadFields(data) {
@@ -35,9 +33,50 @@ export class CharacterSheetComponent implements OnInit {
   }
 
   saveFields() {
-    const owner = (<HTMLInputElement>document.getElementById('character-sheet-owner')).value;
-    const object = (<HTMLInputElement>document.getElementById('character-test-input')).value;
-    this.postMessage(owner, JSON.parse(object));
+    var owner = (<HTMLInputElement>document.getElementById('character-sheet-edit-owner')).value;
+    if(owner) {
+      var object = '';
+      var fields = (<HTMLCollectionOf<Element>>document.getElementsByClassName("field-container"));
+      for( var i = 0; i < fields.length; i++) {
+        var label = (<HTMLInputElement> fields[i].getElementsByClassName('field-label')[0]).value;
+        var value = (<HTMLInputElement> fields[i].getElementsByClassName('field-value')[0]).value;
+        if (label.trim() && value.trim()) {
+          object += '"'+label+'":"'+value+'"';
+          if(i < fields.length-1) {
+            object += ',';
+          }
+        }
+      }
+      this.postMessage(owner, JSON.parse('{'+object+'}'));
+    }
+
+  }
+
+  loadUser() {
+    var owner = (<HTMLInputElement>document.getElementById('character-sheet-owner')).value;
+    this._characterSheetService.getCharacterSheet(owner).subscribe((resData) => this.loadFields(resData));
+  }
+
+  addField() {
+    var fieldsContainer = (<HTMLElement>document.getElementsByClassName("sheet-fields")[0]);
+
+    var fieldContainer = document.createElement("DIV");
+    fieldContainer.classList.add("field-container");
+
+    var labelinput = document.createElement("INPUT");
+    labelinput.classList.add("field-label");
+    labelinput.setAttribute("type", "text");
+    labelinput.setAttribute("placeholder", "Label");
+
+    var valueinput = document.createElement("INPUT");
+    valueinput.classList.add("field-value");
+    valueinput.setAttribute("type", "text");
+    valueinput.setAttribute("placeholder", "Value");
+
+    fieldContainer.appendChild(labelinput);
+    fieldContainer.appendChild(valueinput);
+
+    fieldsContainer.appendChild(fieldContainer);
   }
 
 }
