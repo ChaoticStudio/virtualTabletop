be794eae88ec2b56a98b79c880eaadde9c4af4dd
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2018-07-19 15:49:02 -0300

    header dyna-size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ export class AppComponent {
   title = 'app';
 
   constructor(public _authService: AuthService) { }
+
+  changeHeader(size = 50) {
+    const header = <HTMLDivElement>document.querySelector('.top-bar');
+    const logo = <HTMLImageElement>header.querySelector('.menu img');
+    header.style.maxHeight = size + 'px';
+    logo.style.height = size + 'px';
+  }
 }
