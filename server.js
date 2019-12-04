2b1667c90fc183e3b208a025b4767916cb8b9e43
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2019-12-03 22:54:33 -0300

    Change home Started to refactor the home component

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express'),
     path = require('path'),
     api = require('./server/routes/api'),
     port = 3500,
-    hostName = 'localhost';
+    hostName = '192.168.0.10';
 
 // Where the angular code is placed, gives express access to dist folder
 app.use(express.static(path.join(__dirname, 'dist')));
